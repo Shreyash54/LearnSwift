60d8aa0e5a5fec0d2d75577af45c3b8ebf0e5380
Shreyash Gupta <91052115+Shreyash54@users.noreply.github.com>
2023-06-02 21:14:37 +0530

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,36 @@
+//var num = [1,3,43]
+
+//print("numberfore append   \(num)")
+
+
+//var nums = [12,23,232,344]
+
+
+//num.insert(contentsOf: nums, at: 2)
+
+//print(num)
+
+var nums : Int?
+var someAnotherValue:Int! = 3
+
+if nums != nil{
+  print("the nums is empty \(nums)")
+}
+else{
+  print("doestnty exitst")
+}
+
+if someAnotherValue != nil{
+  print("the vlurs are \(someAnotherValue!)")
+}
+else{
+  print("doesnt exist")
+}
+
+
+
+var a = 12
+var b = 25
+
+var num = a & b
+print(num)
